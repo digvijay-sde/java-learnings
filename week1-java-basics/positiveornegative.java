ab9faa2e03e8e825a772fff481855d2b9b6726d6
Digvijay <workfordvp@gmail.com>
2025-08-16 21:29:43 +0530

    auto: save

---
+++ week1-java-basics/positiveornegative.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class positiveornegative{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int number = sc.nextInt();
+
+        if(number==0){
+            System.out.println("The number is zero,neither positive or negative");
+        }
+        else if(number>0){
+            System.out.println("The number is positive");
+        }
+        else{
+            System.out.println("The number is negative")
+        }
+
+    }
+}
