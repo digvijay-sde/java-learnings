9b80fb52852427b0ab045e994b8c21bddda57d6b
Digvijay <workfordvp@gmail.com>
2025-08-16 21:45:08 +0530

    auto: save

--- week1-java-basics/maxofthree.java
+++ week1-java-basics/maxofthree.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class maxofthree {
      System.out.println("enter your numbers:");
 
-     Scanner sc = new Scanner(S)
+     Scanner sc = new Scanner(System.in)
      int a = sc.nextInt();
      int b = sc.nextInt();
      int c = sc.nextInt();
