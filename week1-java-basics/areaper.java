ae1c8d6488ecbc10bfaa068bcb15967ee0b3c1a8
Digvijay <workfordvp@gmail.com>
2025-08-16 20:37:30 +0530

    auto: save

---
+++ week1-java-basics/areaper.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+import java.lang.path;
+
+public class areaper{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        //area and permiter
+
+        System.out.println("area of circle:");
+        System.out.println("enter radius:");
+
+        dou radius = sc.nextFloat();
+        double pi = Math.PI;
+        float area = pi*radius*radius;
+        float permiter = 2*pi*radius;
+
+        System.out.println("area of circle is"+area+"\nperimter of circle is:"+permiter);
+        
+        
+
+        
+    }
+}
