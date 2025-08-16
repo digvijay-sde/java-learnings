3aa8199eca4d47ae7235980480376efb5aa94aee
Digvijay <workfordvp@gmail.com>
2025-08-16 19:15:18 +0530

    auto: save

--- week1-java-basics/celtofarhn.java
+++ week1-java-basics/celtofarhn.java
@@ -5,7 +5,7 @@ public class celtofarhn {
         
 
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter temperature in celsius");
+        System.out.println("Enter temperature in celsius:");
         
         
     }
