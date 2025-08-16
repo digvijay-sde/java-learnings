3a563cd8a2ba98303f5d0890cc9a9b8e84390615
Digvijay <workfordvp@gmail.com>
2025-08-16 21:17:43 +0530

    auto: save

--- week1-java-basics/oddoreven.java
+++ week1-java-basics/oddoreven.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class oddoreven {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(null)
+        Scanner sc = new Scanner)
         
     }
 }
