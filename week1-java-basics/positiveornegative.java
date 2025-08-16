7facce75511d7cedd7f9bc23f0d08acaa42d4c97
Digvijay <workfordvp@gmail.com>
2025-08-16 21:31:15 +0530

    auto: save

--- week1-java-basics/positiveornegative.java
+++ week1-java-basics/positiveornegative.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class positiveornegative{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your number:");
 
         int number = sc.nextInt();
 
