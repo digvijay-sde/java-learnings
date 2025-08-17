bcc0660f859cb15fcf9e3df482c21c4723f0690b
Digvijay <workfordvp@gmail.com>
2025-08-17 19:08:29 +0530

    auto: save

--- week1-java-basics/leapyearcheck.java
+++ week1-java-basics/leapyearcheck.java
@@ -1,7 +1,7 @@
 public class leapyearcheck {
     public static void main(String[] args) {
         System.out.println("enter your year:");
-        Scanner sc = ne
+        Scanner sc = new Scanner
 
     }
 }
