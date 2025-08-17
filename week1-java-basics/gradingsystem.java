492748551a6ffc4e2cb950e0935a4d43476f8c32
Digvijay <workfordvp@gmail.com>
2025-08-17 19:24:12 +0530

    auto: save

--- week1-java-basics/gradingsystem.java
+++ week1-java-basics/gradingsystem.java
@@ -29,6 +29,6 @@ public class gradingsystem {
         else{
             System.out.println("Enter valid marks");
         }
-        System.out.println("");
+        System.out.println("/n for");
     }
 }
