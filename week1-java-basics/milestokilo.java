33acc198057103d4b3e5aa2a1b964fd890c195b5
Digvijay <workfordvp@gmail.com>
2025-08-17 18:57:25 +0530

    auto: save

--- week1-java-basics/milestokilo.java
+++ week1-java-basics/milestokilo.java
@@ -3,6 +3,7 @@ public class milestokilo {
     public static void main(String[] args) {
         System.out.println("enter distance in miles:");
         Scanner sc = new Scanner(System.in);
+
         
     }
 }
