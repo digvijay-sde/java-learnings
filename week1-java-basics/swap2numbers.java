f551928235b7f8decc848ee6cd03e2dd5bd84c1a
Digvijay <workfordvp@gmail.com>
2025-08-17 18:53:35 +0530

    auto: save

--- week1-java-basics/swap2numbers.java
+++ week1-java-basics/swap2numbers.java
@@ -4,5 +4,7 @@ public class swap2numbers {
     public static void main(String[] args) {
         System.out.println("enter two numbers:");
         Scanner sc = new Scanner(System.in);
+
+        
     }
 }
