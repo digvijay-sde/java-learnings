fac3ce9095743f346ed176f13973ed9702825bb4
Digvijay <workfordvp@gmail.com>
2025-08-17 21:25:48 +0530

    auto: save

--- week2-java/buzznumber.java
+++ week2-java/buzznumber.java
@@ -8,7 +8,7 @@ public class buzznumber {
         System.out.println("enter your number:");
 
         int number = sc.nextInt();
-        String str = String
+        String str = String.valueof
 
     }
 }
