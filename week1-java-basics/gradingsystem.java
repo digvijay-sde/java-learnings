743c1d3128f409e0377aad59d0cdfbb753e0c08c
Digvijay <workfordvp@gmail.com>
2025-08-17 19:20:48 +0530

    auto: save

--- week1-java-basics/gradingsystem.java
+++ week1-java-basics/gradingsystem.java
@@ -6,7 +6,7 @@ public class gradingsystem {
         Scanner sc = new Scanner(System.in);
         System.out.println("Grading system:\n"+"Enter your marks percentage:");
 
-        float marks = sc.nextFloat();
+        float marks = sc.nextFloat(+);
         if (marks>=90) {
             System.out.println("Grade A");
             
