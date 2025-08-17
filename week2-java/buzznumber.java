7c1f628518d349288137a841204e49ed70c3fffd
Digvijay <workfordvp@gmail.com>
2025-08-17 20:26:07 +0530

    auto: save

--- week2-java/buzznumber.java
+++ week2-java/buzznumber.java
@@ -2,6 +2,6 @@ public class buzznumber {
 
     public static void main(String[] args) {
         // buzz number is a number that ends with 7 or is divible by 7
-        
+        Scanner sc = new S
     }
 }
