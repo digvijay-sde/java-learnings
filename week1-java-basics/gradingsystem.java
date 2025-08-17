848d827b26cf02b26396b9e6deae5b0cea23800c
Digvijay <workfordvp@gmail.com>
2025-08-17 19:18:56 +0530

    auto: save

--- week1-java-basics/gradingsystem.java
+++ week1-java-basics/gradingsystem.java
@@ -1,6 +1,7 @@
 public class gradingsystem {
 
     public static void main(String[] args) {
-        Scanner sc - new Scanner(System.in)
+        Scanner sc - new Scanner(System.in);
+        
     }
 }
