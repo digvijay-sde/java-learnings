878b7cf8be6aece1eb3b122abd668ac406aac63d
Digvijay <workfordvp@gmail.com>
2025-08-17 19:42:47 +0530

    auto: save

--- week1-java-basics/divisibleby5.java
+++ week1-java-basics/divisibleby5.java
@@ -1,7 +1,9 @@
+import java.util.Scanner;
+
 public class divisibleby5 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int number = sc.
+        int number = sc.nextInt()
     }
 }
